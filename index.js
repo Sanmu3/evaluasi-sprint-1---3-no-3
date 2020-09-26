2a71b5a647c8b32e09bb11598b3e48ec4d3e23f2
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-26 13:32:19 +0700

    evaluasi 1-3

---
+++ index.js
@@ -0,0 +1,9 @@
+var panjang = prompt('masukan panjang')
+var lebar = prompt('masukan lebar')
+
+function LuasPersegiPanjang(panjang, lebar) {
+   return luas = panjang * lebar
+}
+document.write('panjang : ' + panjang + '<br/>')
+document.write('lebar : '+ lebar + '<br/>')
+document.write('<br/>luas persegi panjang : ' + LuasPersegiPanjang(panjang, lebar))
